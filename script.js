c6f964dd948de5697133c4824d3794955cb7db13
tosin-t <tosin100@homail.com>
2023-10-31 01:14:41 +0000

    finished code

--- script.js
+++ script.js
@@ -88,9 +88,9 @@ var upperCasedCharacters = [
   'Z'
 ];
 // starting promt
-
+userinput='';
 var startq = prompt("How many characters would you like your password to contain? Please enter an integer between 8-128");
-
+console.log(startq)
 if (startq == null) {
   alert("That's a shame, please refresh the page to restart");
 } else {
@@ -100,9 +100,9 @@ if (startq == null) {
     // special charatcer
   } else {
     var specialcharactersQ = confirm("Do you want special characters?");
-    
     if (specialcharactersQ) {
       alert("Special character added");
+      userinput += specialCharacters.join('');
     } else {
       alert("Special character not added");
     }
@@ -110,13 +110,16 @@ if (startq == null) {
     var lowercase = confirm("Do you want lowercase characters?");
     if (lowercase) {
       alert("Lowercase character added");
+      userinput += lowerCasedCharacters.join('');
+      console.log(userinput)
     } else {
       alert("Lowercase character not added");
     }
 //  upper case character choice
-    var uppercasecase = confirm("Do you want lowercase characters?");
+    var uppercase = confirm("Do you want Uppercase characters?");
     if (uppercase) {
       alert("Uppercase character added");
+      userinput += upperCasedCharacters.join('');
     } else {
       alert("Uppercase character not added");
     }
@@ -124,30 +127,49 @@ if (startq == null) {
     var numbers = confirm("Do you want numeric characters?");
     if (numbers) {
       alert("numeric characters added");
+      userinput += numericCharacters.join('');
+      console.log(userinput)
     } else {
       alert("numeric characters not added");
     }
 
   }
+  // Function to generate password with user input
+function createPassword(length, characters) {
+  var actualpassword = '';
+  for (var i = 0; i < length; i++) {
+    var randomiser = Math.floor(Math.random() * characters.length);
+    actualpassword += characters[randomiser];
+  }
+  return actualpassword;
+}
+
+  if (userinput.length > 0) {
+    var passwordR = createPassword(startq, userinput);
+    console.log (passwordR)
+  } else {
+    alert("You must select at least one character set. Please refresh and try again.");
+  }
+  
 }
 
 
 
 
-// Function to prompt user for password options
+// // Function to prompt user for password options
 function getPasswordOptions() {
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-Math.floor(math.random())
+
 }
+
 // Function to generate password with user input
 function generatePassword() {
 
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -157,8 +179,9 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  passwordText.value = passwordR;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
